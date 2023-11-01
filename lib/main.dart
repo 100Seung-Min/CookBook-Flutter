bb170719c674718eeeeb40eb41857f2bf357c8f7
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-11-02 08:57:55 +0900

    localSource 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:cookbook/core/navigation/navigator.dart';
 import 'package:cookbook/core/provider/bloc_provider.dart';
+import 'package:cookbook/core/provider/local_source_provider.dart';
 import 'package:cookbook/core/provider/network_provider.dart';
 import 'package:cookbook/core/provider/remote_source_provider.dart';
 import 'package:cookbook/core/provider/repository_provider.dart';
+import 'package:cookbook/data/local/datasource/food_local_source.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FoodLocalSource.initialize();
+
   runApp(
     MultiRepositoryProvider(
       providers: [
         networkProvider,
+        ...localSourceProvider,
         ...remoteSourceProvider,
         ...repositoryProvider
       ],
